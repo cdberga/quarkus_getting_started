2797b30707b12f50b9fa8b8c4ed87d34bcf06291
Carlos <carlos.bergamasco@gmail.com>
2019-06-30 17:17:35 -0300

    Commit inicial

---
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -0,0 +1,38 @@
+package org.acme.quickstart;
+
+import io.quarkus.test.common.http.TestHTTPResource;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.net.URL;
+import java.util.UUID;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @TestHTTPResource("/myservlet")
+    URL testUrl;
+    
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(is("hello " + uuid + "\n"));
+    }
+}
